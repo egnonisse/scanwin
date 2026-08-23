f844b21bfef27a2a846bef6a02dc0f507fdebbc2
egnonisse <egnonisse@gmail.com>
2026-08-23 00:40:47 +0000

    chore: registrant macOS régénéré (package_info_plus)

--- ticket_scanner/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ticket_scanner/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_messaging
 import firebase_storage
 import geolocator_apple
 import mobile_scanner
+import package_info_plus
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -29,6 +30,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
